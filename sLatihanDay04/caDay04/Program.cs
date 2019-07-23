11ecc14db850f629c910923537434311929280a1
Wiwiet Widyaningsih <wiwietwidyaningsih27@gmail.com>
2019-07-23 14:46:09 +0700

    first commit

---
+++ sLatihanDay04/caDay04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caDay04
+{
+    class Program
+    {
+
+        public static int hitung(int x)
+        {
+            return x + 2;
+        }
+        static void Main(string[] args)
+        {
+            int angka = 3;
+            int angka2 = hitung(angka);
+            bool status = true;
+
+            Console.Write("Selamat Datang {0} {1} {2}\n ", angka, status, angka2);
+            Console.Write("Selamat Datang "+ angka+" "+status);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sLatihanDay04/caSimpleArraySum/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caSimpleArraySum
+{
+    class Program
+    {
+        static int simpleArraySum(string[] ar, int maxValue)
+        {
+            /*
+             * Write your code here.
+             */
+
+            int hasil = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (maxValue == hasil)
+                {
+                    break;
+                }
+                else
+                {
+                    hasil += Convert.ToInt32(ar[i]);
+                }
+                
+            }
+
+            return hasil;
+
+        }
+        static void Main(string[] args)
+        {
+        
+            int arCount = Convert.ToInt32(Console.ReadLine());
+            int[] ary = new int[arCount];
+            int result = 0;
+            int ctr = 0;
+            //Console.WriteLine(arCount);
+            string bebas = Console.ReadLine();
+            //Console.WriteLine(bebas);
+            string[] ar = bebas.Split(' ');
+            Console.WriteLine(simpleArraySum(ar, arCount));
+
+            
+
+
+            //foreach (var item in ar)
+            //{
+            //    //Console.WriteLine(item);
+            //    ary[ctr] = Convert.ToInt32(item);
+            //    ctr++;
+
+            //}
+            //foreach (var item in ary)
+            //{
+            //    result = result + item;
+            //}
+
+            // simpleArraySum(ar);
+
+            //Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
